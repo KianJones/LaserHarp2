a1da02099cc6082ce53a22a7177bc5f74c043d53
Kian Jones <kian.jones95@gmail.com>
2014-03-25 03:50:36 +0000

    got rid of huge comment block

--- read_photo_and_light_led/read_photo_and_light_led.ino
+++ read_photo_and_light_led/read_photo_and_light_led.ino
@@ -46,20 +46,6 @@ void loop(void) {
   Serial.print(G_reading);
   Serial.println();*/
  
-  // We'll have a few threshholds, qualitatively determined
-  /*if (photocellReading < 10) {
-    Serial.println(" - Dark");
-  } else if (photocellReading < 200) {
-    Serial.println(" - Dim");
-  } else if (photocellReading < 500) {
-    Serial.println(" - Light");
-    flash(LEDpin);
-  } else if (photocellReading < 800) {
-    Serial.println(" - Bright");
-    flash(LEDpin);
-  } else {
-    Serial.println(" - Very bright");
-  }*/
   if(R_reading == 0){
     //Serial.println("off");
   }else{
